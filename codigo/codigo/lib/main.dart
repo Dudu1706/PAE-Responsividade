f19ea580d431c5ab598d311534135c7e77d56224
Eduardo Aguiar Leite da Silva <160495392+Dudu1706@users.noreply.github.com>
2024-05-12 22:04:13 -0300

    Update main.dart

--- codigo/codigo/lib/main.dart
+++ codigo/codigo/lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:codigo/app.dart';
-
+// RA: 24.00380-8. Eduardo Aguiar Leite da Silva 
+// RA: 24.00947-4. Victor Hugo Gaetano Oliveira Tavares de Pinho
 void main() {
   runApp(const App());
 }
